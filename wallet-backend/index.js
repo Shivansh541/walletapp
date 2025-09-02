3ea55548035731e34b52e7ef064331cea3932471
Shivansh Singh Rathore <rathoreshivansh54@gmail.com>
2025-09-02 16:02:26 +0530

    added atlas

--- wallet-backend/index.js
+++ wallet-backend/index.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const connectDB = require("./config/db");
 const authRoutes = require("./routes/auth");
 const walletRoutes = require("./routes/wallet");
 const adminRoutes = require("./routes/admin");
 require("./jobs/scheduler"); // run cron jobs
+
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error(error.message);
+    process.exit(1);
+  }
+};
 
 dotenv.config();
 connectDB();
